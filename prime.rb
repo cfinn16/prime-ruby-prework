0779a0fcccde3c425e0a67f1c078077f2b506017
Connor Finnegan <connorfinnegan16@gmail.com>
2018-10-11 18:18:25 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(integer) 
+  array = (1..integer).to_a 
+  
+  i = 0
+  while array[i] <= integer
+    if integer % array[i] == 0
+      return true 
+    else
+      i += 1 
+  end
+
+return false     
+end
