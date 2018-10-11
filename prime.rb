95a146809585158046dcccac98dc1e3049b72237
Connor Finnegan <connorfinnegan16@gmail.com>
2018-10-11 18:22:14 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,7 @@
 def prime?(num) 
   a = Array (1..num)
   
-  i = 0
-  while array[i] <= integer
-    if integer % array[i] == 0
-      return true 
-    else
-      i += 1 
-  end
+  
 
 return false     
 end
