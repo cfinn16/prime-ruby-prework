d4dfa61c467955e577554b4b17404a6cd04aa101
Connor Finnegan <connorfinnegan16@gmail.com>
2018-10-11 18:32:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,11 @@
 def prime?(num) 
-  a = Array (1..num -1)
+  a = Array (1..(num - 1))
   
   a.each do |int|
     if num % int == 0 
       return false 
     end
-  return true  
   end
+  
+return true
 end
